8a190277f80c55ed48227979c504c0c4131cbab7
Minion <nessakristo@gmail.com>
2022-10-26 12:37:44 +0100

    Added map and other charts

---
+++ assets/js/controller.js
@@ -0,0 +1,154 @@
+let format_day = document.getElementById("format_day");
+let format_month = document.getElementById("format_month");
+let filter_type = document.getElementById("type");
+let type = document.getElementById("type");
+let format = document.getElementsByName("format");
+let start_date = document.getElementById("start_date");
+let end_date = document.getElementById("end_date");
+let table_data = document.getElementById("table_data");
+let table_head = document.getElementById("table_head");
+
+const URL = "https://62ffa49234344b6431fe43fc.mockapi.io/statistics"
+
+
+window.onload = () => {
+    let start_date_new = start_date.value
+    let end_date_new = end_date.value
+
+    if (start_date_new == "") {
+        start_date_new = end_date_new
+    } else if (end_date_new == "") {
+        end_date_new = start_date_new
+    }
+
+    let format_value;
+
+    for (let i = 0; i < format.length; i++) {
+        if (format[i].checked) {
+            format_value = format[i].value;
+        }
+    }
+
+    if (format_value == "month" || format_value == "day") {
+        fetchData(URL, format_value, start_date_new, end_date_new, table_data, table_head, type.value);
+    } else {
+        let today = new Date();
+        let dd = String(today.getDate()).padStart(2, '0');
+        let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+        let yyyy = today.getFullYear();
+
+        today = yyyy + '-' + mm + '-' + dd;
+
+        // Default chart
+        start_date.value = today;
+        end_date.value = today;
+        format_month.checked = true;
+
+        fetchData(URL, "month", start_date.value, end_date.value, table_data, table_head, "signup");
+    }
+
+    format_month.addEventListener("click", () => {
+        let start_date_new = start_date.value
+        let end_date_new = end_date.value
+
+        if (start_date_new == "") {
+            start_date_new = end_date_new
+        } else if (end_date_new == "") {
+            end_date_new = start_date_new
+        }
+
+        let format_value;
+
+        for (let i = 0; i < format.length; i++) {
+            if (format[i].checked) {
+                format_value = format[i].value;
+            }
+        }
+
+        fetchData(URL, format_value, start_date_new, end_date_new, table_data, table_head, type.value);
+    });
+
+    format_day.addEventListener("click", () => {
+        let start_date_new = start_date.value
+        let end_date_new = end_date.value
+
+        if (start_date_new == "") {
+            start_date_new = end_date_new
+        } else if (end_date_new == "") {
+            end_date_new = start_date_new
+        }
+
+        let format_value;
+
+        for (let i = 0; i < format.length; i++) {
+            if (format[i].checked) {
+                format_value = format[i].value;
+            }
+        }
+
+        fetchData(URL, format_value, start_date_new, end_date_new, table_data, table_head, type.value);
+    });
+
+    type.addEventListener("change", () => {
+        let start_date_new = start_date.value
+        let end_date_new = end_date.value
+
+        if (start_date_new == "") {
+            start_date_new = end_date_new
+        } else if (end_date_new == "") {
+            end_date_new = start_date_new
+        }
+
+        let format_value;
+
+        for (let i = 0; i < format.length; i++) {
+            if (format[i].checked) {
+                format_value = format[i].value;
+            }
+        }
+
+        fetchData(URL, format_value, start_date_new, end_date_new, table_data, table_head, type.value);
+    });
+
+    start_date.addEventListener("change", () => {
+        let start_date_new = start_date.value
+        let end_date_new = end_date.value
+
+        if (start_date_new == "") {
+            start_date_new = end_date_new
+        } else if (end_date_new == "") {
+            end_date_new = start_date_new
+        }
+
+        let format_value;
+
+        for (let i = 0; i < format.length; i++) {
+            if (format[i].checked) {
+                format_value = format[i].value;
+            }
+        }
+
+        fetchData(URL, format_value, start_date_new, end_date_new, table_data, table_head, type.value);
+    });
+
+    end_date.addEventListener("change", () => {
+        let start_date_new = start_date.value
+        let end_date_new = end_date.value
+
+        if (start_date_new == "") {
+            start_date_new = end_date_new
+        } else if (end_date_new == "") {
+            end_date_new = start_date_new
+        }
+
+        let format_value;
+
+        for (let i = 0; i < format.length; i++) {
+            if (format[i].checked) {
+                format_value = format[i].value;
+            }
+        }
+
+        fetchData(URL, format_value, start_date_new, end_date_new, table_data, table_head, type.value);
+    });
+}
---
+++ assets/js/main.js
@@ -0,0 +1,354 @@
+// Load google API
+google.charts.load("current", {
+  packages: ["corechart"],
+});
+
+function fetchData(
+  url,
+  format,
+  start_date,
+  end_date,
+  table_data,
+  table_head,
+  type
+) {
+  let xhttp = new XMLHttpRequest();
+
+  // Make AJAX call
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      let entry = JSON.parse(this.responseText);
+      run(
+        entry,
+        format,
+        start_date,
+        end_date,
+        table_data,
+        table_head,
+        type
+      );
+    } else {
+      document.getElementById("line_div").innerHTML = `<div class="d-flex justify-content-center">
+      <div class="spinner-border text-light" style="margin-top: 5rem; width: 4rem; height: 4rem" role="status">
+          <span class="visually-hidden">Loading...</span>
+      </div></div>`;
+      document.getElementById("bar_div").innerHTML = `<div class="d-flex justify-content-center">
+      <div class="spinner-border text-light" style="margin-top: 5rem; width: 4rem; height: 4rem" role="status">
+          <span class="visually-hidden">Loading...</span>
+      </div></div>`;
+
+      document.getElementById("table_header").innerHTML = ""
+      table_head.innerHTML = "";
+      table_data.innerHTML = "";
+      document.getElementById("total").innerHTML = `...`
+    }
+  };
+
+  xhttp.open("GET", url, true);
+  xhttp.send();
+}
+
+function filter_months(entry, start_date, end_date, type) {
+  const month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  let filter_data = [];
+
+  if (type == "available") {
+    let previous_months_count = 0;
+
+    entry.forEach((element) => {
+      let search_month = new Date(element.date).toLocaleDateString()
+      let start_month_modified = new Date(new Date(start_date).getFullYear(), new Date(start_date).getMonth(), 1).toLocaleDateString()
+      let end_month_modified = new Date(new Date(end_date).getFullYear(), new Date(end_date).getMonth() + 1, 0).toLocaleDateString()
+
+      if (
+        new Date(search_month) >= new Date(start_month_modified) &&
+        new Date(search_month) <= new Date(end_month_modified) &&
+        element.type == type
+      ) {
+        filter_data.push(month[new Date(element.date).getMonth()]);
+      }
+
+      if (
+        new Date(search_month) < new Date(start_month_modified) &&
+        element.type == type
+      ) {
+        previous_months_count += 1;
+      }
+    });
+
+    let data = {};
+
+    filter_data.forEach((element) => {
+      data[element] = (data[element] || 0) + 1;
+    });
+
+    let result = Object.keys(data).sort(function (a, b) {
+      return month.indexOf(a) > month.indexOf(b);
+    }).map((key, index, array) => {
+      previous_months_count += (data[array[index - 1]] || 0)
+      return [key, data[key] + previous_months_count];
+    });
+
+    return result;
+  } else {
+    entry.forEach((element) => {
+      let search_month = new Date(element.date).toLocaleDateString()
+      let start_month_modified = new Date(new Date(start_date).getFullYear(), new Date(start_date).getMonth(), 1).toLocaleDateString()
+      let end_month_modified = new Date(new Date(end_date).getFullYear(), new Date(end_date).getMonth() + 1, 0).toLocaleDateString()
+
+      if (
+        new Date(search_month) >= new Date(start_month_modified) &&
+        new Date(search_month) <= new Date(end_month_modified) &&
+        element.type == type
+      ) {
+        filter_data.push(month[new Date(element.date).getMonth()]);
+      }
+    });
+
+    let data = {};
+
+    filter_data.forEach((element) => {
+      data[element] = (data[element] || 0) + 1;
+    });
+
+    let result = Object.keys(data).sort(function (a, b) {
+      return month.indexOf(a) > month.indexOf(b);
+    }).map((key) => {
+      return [key, data[key]];
+    });
+
+    return result;
+  }
+}
+
+function filter_days(entry, start_date, end_date, type) {
+  let filter_data = [];
+
+  entry.forEach((element) => {
+    let search_date = new Date(element.date).toLocaleDateString()
+    let start_date_modified = new Date(start_date).toLocaleDateString()
+    let end_date_modified = new Date(end_date).toLocaleDateString()
+
+    if (
+      new Date(search_date) >= new Date(start_date_modified) &&
+      new Date(search_date) <= new Date(end_date_modified) &&
+      element.type == type
+    ) {
+      filter_data.push(new Date(element.date).toDateString());
+    }
+  });
+  let data = {};
+
+  filter_data.forEach((element) => {
+    data[element] = (data[element] || 0) + 1;
+  });
+
+  let result = Object.keys(data).sort(function (a, b) {
+    return new Date(a) > new Date(b);
+  }).map((key) => {
+    return [key, data[key]];
+  });
+
+  return result;
+}
+
+function filter(
+  entry,
+  format,
+  type,
+  start_date,
+  end_date
+) {
+  if (format == "month") {
+    return filter_months(entry, start_date, end_date, type);
+  } else if (format == "day") {
+    return filter_days(entry, start_date, end_date, type);
+  }
+}
+
+function line(data) {
+  if (data.length < 2) {
+    document.getElementById("line_div").innerHTML = `<h5 class="text-danger text-center ">Sorry No Data To Display!</h5>`
+  } else {
+    // Set a callback to run when the Google Visualization API is loaded.
+    google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+      var result = google.visualization.arrayToDataTable(data);
+
+      // Set chart options
+      var options = {
+        vAxis: {
+          title: data[0][1],
+          format: "0",
+          minValue: 0,
+          textStyle:{color: '#FFF'}
+        },
+        hAxis: {
+          title: data[0][0],
+          textStyle:{color: '#FFF'}
+        },
+        title: `${data[0][1]} METRICS`,
+        height: 250,
+        backgroundColor: '#0e213b',
+        legendTextStyle: {
+          color: '#FFF'
+        },
+        titleTextStyle: {
+          color: '#FFF'
+        },
+      };
+
+      // Instantiate and draw our chart, passing in some options.
+      var chart = new google.visualization.LineChart(
+        document.getElementById("line_div")
+      );
+
+      chart.draw(result, options);
+    }
+  }
+}
+
+function bar(data) {
+  if (data.length < 2) {
+    document.getElementById("bar_div").innerHTML = `<h5 class="text-danger text-center ">Sorry No Data To Display!</h5>`
+  } else {
+    // Set a callback to run when the Google Visualization API is loaded.
+    google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+      var result = google.visualization.arrayToDataTable(data);
+
+      // Set chart options
+      var options = {
+        vAxis: {
+          title: data[0][1],
+          format: "0",
+          minValue: 0,
+          textStyle:{color: '#FFF'}
+        },
+        hAxis: {
+          title: data[0][0],
+          textStyle:{color: '#FFF'}
+        },
+        title: `${data[0][1]} METRICS`,
+        height: 250,
+        backgroundColor: '#0e213b',
+        legendTextStyle: {
+          color: '#FFF'
+        },
+        titleTextStyle: {
+          color: '#FFF'
+        },
+      };
+      // Instantiate and draw our chart, passing in some options.
+      var chart = new google.visualization.BarChart(
+        document.getElementById("bar_div")
+      );
+
+      chart.draw(result, options);
+    }
+  }
+}
+
+function pie(data) {
+  if (data.length < 2) {
+    document.getElementById("pie_div").innerHTML = `<h5 class="text-danger text-center ">Sorry No Data To Display!</h5>`
+  } else {
+    // Set a callback to run when the Google Visualization API is loaded.
+    google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+      var result = google.visualization.arrayToDataTable(data);
+
+      // Set chart options
+      var options = {
+        vAxis: {
+          title: data[0][1],
+          format: "0",
+          minValue: 0,
+          textStyle:{color: '#FFF'}
+        },
+        hAxis: {
+          title: data[0][0],
+          textStyle:{color: '#FFF'}
+        },
+        title: `${data[0][1]} METRICS`,
+        height: 250,
+        backgroundColor: '#0e213b',
+        legendTextStyle: {
+          color: '#FFF'
+        },
+        titleTextStyle: {
+          color: '#FFF'
+        },
+      };
+      // Instantiate and draw our chart, passing in some options.
+      var chart = new google.visualization.PieChart(
+        document.getElementById("pie_div")
+      );
+
+      chart.draw(result, options);
+    }
+  }
+}
+
+function run(
+  data,
+  format,
+  start_date,
+  end_date,
+  table_data,
+  table_head,
+  type
+) {
+  let headers = [format.toUpperCase(), type.toUpperCase()];
+  let filter_data = filter(
+    data,
+    format,
+    type,
+    start_date,
+    end_date
+  );
+
+  // table
+  document.getElementById("table_header").innerHTML = `<h6 class="text-light">Summary Table</h6>`
+  table_head.innerHTML = `<tr><th scope="col">${headers[0]}</th><th scope="col">${headers[1]}</th></tr>`;
+  table_data.innerHTML = "";
+
+  let total = 0;
+
+  filter_data.forEach((item, index, array) => {
+    table_data.innerHTML += `<tr><td>${item[0]}</td><td>${item[1]}</td></tr>`;
+
+    if (type == "available") {
+      if (index == array.length - 1) {
+        total = item[1];
+      }
+    } else {
+      total += item[1];
+    }
+  });
+
+  document.getElementById("total").innerHTML = total
+
+  filter_data.unshift(headers);
+
+  line(filter_data);
+  bar(filter_data);
+  pie(filter_data);
+}
